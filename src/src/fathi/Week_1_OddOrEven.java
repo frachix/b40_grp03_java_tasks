6c12f7f78f95e76d351ecafeab02f3e503d67a72
Rachid <fathirachid995@gmail.com>
2025-08-03 15:10:18 +0100

    Team here is the updated OddOrEven and solving first week task.

--- src/src/fathi/Week_1_OddOrEven.java
+++ src/src/fathi/Week_1_OddOrEven.java
@@ -1,8 +1,52 @@
 package src.fathi;
 
 public class Week_1_OddOrEven {
+
+
+
     public static void main(String[] args) {
 
-        int num = 5;
+
+
+        int num=0;
+
+        if (num%2==0){
+            System.out.println();
+            System.out.println("Number is Even:" + num);
+        }else {
+            System.out.println();
+            System.out.println("Number is Odd:" + num);
+        }
+
     }
 }
+
+
+
+/*
+package src.michael;
+
+public class Week_01_OddOrEven {
+
+
+    /*
+    *  Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+
+public static void main(String[] args) {
+    int num=0;
+
+    if (num%2==0){
+        System.out.println();
+        System.out.println("Number is Even:" + num);
+    }else {
+        System.out.println();
+        System.out.println("Number is Odd:" + num);
+    }
+
+}
+}
+ */
